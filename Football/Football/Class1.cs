9bbff258a78f13d44fcb6ca8186f96a35419d912
andrewbolek <46454105+andrewbolek@users.noreply.github.com>
2019-01-22 07:56:35 -0500

    Add files via upload

---
+++ Football/Football/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football
+{
+    public class Team
+    {
+        public bool? Win { get; set; } // Property
+    }
+
+    public class Alabama : Team
+    {
+        public Alabama()
+        { Win = false; }
+    }
+
+    public class Clemson : Team
+    {
+        public Clemson()
+        { Win = true; }
+
+
+    }
+}
+
+
+        
+   
+        
+  
+
+   
+
---
+++ Football/Football/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football
+{
+    class Program
+
+    {
+        static void Main(string[] args)           
+        {
+            Clemson Taylor = new Clemson();
+            Taylor.Win = true;
+
+            Alabama Tua = new Alabama();
+            Tua.Win = false;
+            if(Taylor.Win==true)
+            {
+                Console.WriteLine("Victory Clemson!");
+
+                    }
+            else
+            { Console.WriteLine("Victory Alabama..");
+            }
+            Console.ReadLine();
+
+        }
+    }
+
+}
+  
+
